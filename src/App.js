26b88439aa56903832b93bc560a0ac5b732107d9
prLeonard <99710567+prLeonard@users.noreply.github.com>
2022-03-01 20:42:17 -0800

    Update App.js
    Remove auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AmplifySignOut />
+        {/*<AmplifySignOut*/}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -30,4 +30,4 @@ class App extends Component {
   }
 }
 
-export default withAuthenticator(App);
+export default App;

1065860cf31d82e8034ff7598a1f18b9ccdbdf1b
prLeonard <99710567+prLeonard@users.noreply.github.com>
2022-03-04 14:13:41 -0800

    Probable fix?

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 // import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
-// import aws_exports from './aws-exports';
-// Amplify.configure(aws_exports);
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
